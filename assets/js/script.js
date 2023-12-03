9a10835130c9a4415c9d77c38862855ffcc30c07
Zaniar Karimi <105171640+I-am-Programming-the-World@users.noreply.github.com>
2023-12-03 23:38:39 +0330

    Added the necessary JS code for activating the "Nav-Bar Toggle".

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
+// Navbar variables
+const nav = document.querySelector(".mobile-nav"),
+    navMenuBtn = document.querySelector(".nav-menu-btn"),
+    navCloseBtn = document.querySelector(".nav-close-btn");
 
+// A navigation toggle function is declared
+const navToggleFunction = () => nav.classList.toggle("active");
+navMenuBtn.addEventListener("click", navToggleFunction);
+navCloseBtn.addEventListener("click", navToggleFunction);
 /* This program was developed by ©I-am-Programming-the-World */
