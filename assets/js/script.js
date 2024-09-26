a5d6dfe493f9e78433bd1604f33dec4d64d5f9fc
zaniarkarimi <zaniarkarimi@chmail.ir>
2024-09-26 16:32:18 +0330

    Added dark mode!

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,19 @@ const nav = document.querySelector(".mobile-nav"),
 const navToggleFunction = () => nav.classList.toggle("active");
 navMenuBtn.addEventListener("click", navToggleFunction);
 navCloseBtn.addEventListener("click", navToggleFunction);
+
+// Theme toggle variables
+const themeBtn = document.querySelectorAll(".theme-btn");
+
+for (let i = 0; i < themeBtn.length; i++) {
+  themeBtn[i].addEventListener("click", function () {
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
+
+    for (let j = 0; j < themeBtn.length; j++) {
+      themeBtn[j].classList.toggle("light");
+      themeBtn[j].classList.toggle("dark");
+    }
+  });
+}
 /* This program was developed by ©I-am-Programming-the-World */
